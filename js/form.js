e352aeb94f0494a3f9ea569374f491203c14a49a
Tejas-creator <76105346+Tejas-creator@users.noreply.github.com>
2021-08-15 18:50:19 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -14,21 +14,21 @@ class Form {
 	}
 	display() {
 		this.title.html('FRUIT CATCHER');
-		this.title.position(680, 50);
+		this.title.position(580, 50);
 		this.title.style('font-size', '70px');
 		this.title.style('color', 'cyan');
 
-		this.input.position(880, 400);
+		this.input.position(580, 400);
 		this.input.style('width', '200px');
 		this.input.style('height', '20px');
 		this.input.style('background', 'lavender');
 
-		this.button.position(880, 500);
+		this.button.position(580, 500);
 		this.button.style('width', '200px');
 		this.button.style('height', '40px');
 		this.button.style('background', 'lightblue');
 
-		this.reset.position(1350, 660);
+		this.reset.position(900, 660);
 		this.reset.style('width', '100px');
 		this.reset.style('height', '30px');
 		this.reset.style('background', 'red');
